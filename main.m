334209efe81dda1bcc96e10897e9263a15d64359
Matt Mower <self@mattmower.com>
2008-09-01 19:58:18 +0100

    Added project files from Tomas' sample project

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  StackedListView
+//
+//  Created by Tomas Franzén on 2008-07-16.
+//  Copyright Lighthead Software 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
